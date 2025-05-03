9f5cb05cf37602076b22cdae96a156d20b39f34a
honghungluong-asus <luonghonghung95@gmail.com>
2025-05-03 15:57:03 +0700

    add health check

---
+++ DockerTestDeploy/Controllers/Health-CheckController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerTestDeploy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Health_CheckController : ControllerBase
+    {
+    }
+}
---
+++ DockerTestDeploy/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerTestDeploy.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+{
+            "Health check successfully!"
+        };
+
+        [HttpGet(Name = "Health-Check")]
+        public string Get()
+        {
+            return Summaries[0];
+        }
+    }
+}
